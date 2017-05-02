c12ed1f18624adff103bbbdec7e6c4d6038becf7
PutraNasri <nasrisyawaldi@gmail.com>
2017-05-02 17:15:44 +0700

    tinggal isi berita yeaaa

--- app/src/main/java/com/kuena/kinket/berita/IsiBerita.java
+++ app/src/main/java/com/kuena/kinket/berita/IsiBerita.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class IsiBerita extends AppCompatActivity {
-
+    private String judul;
+    private String image;
+    private String isi;
+    private String tanggal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
